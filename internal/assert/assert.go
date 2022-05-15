167d6c1077ca37133fd3bc9f92d6be3db5834bfd
Kia Farhang <kfarhang0@gmail.com>
2022-05-15 12:22:33 -0500

    doc: comment assert package

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,7 +1,15 @@
+/**
+Package assert implements simple assertion functions for use in tests.
+
+TODO: in reality we'd probably want a library like testify, but this is fine
+for a proof of concept.
+*/
 package assert
 
 import "testing"
 
+// Equal compares two comparables of the same type and calls t.Errorf if
+// they are not equal.
 func Equal[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got != want {
@@ -9,6 +17,7 @@ func Equal[T comparable](t *testing.T, got, want T) {
 	}
 }
 
+// NoError calls t.Errorf if the err passed is not nil.
 func NoError(t *testing.T, err error) {
 	t.Helper()
 	if err != nil {
