9f5668b2ed0c3490a723860f03136992913e16e4
Kia Farhang <kfarhang0@gmail.com>
2022-05-16 18:14:32 -0500

    feature: send client admin cert from client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -12,6 +12,12 @@ import (
 )
 
 func main() {
+	clientCert, err := tls.LoadX509KeyPair("certs/client/admin.crt", "certs/client/client.key")
+
+	if err != nil {
+		log.Fatalf("Error loading client cert: %s", err.Error())
+	}
+
 	caCert, err := ioutil.ReadFile("certs/ca/CA.pem")
 	if err != nil {
 		log.Fatalf("Error reading CA cert file: %s", err.Error())
@@ -20,7 +26,7 @@ func main() {
 	caCertPool := x509.NewCertPool()
 	caCertPool.AppendCertsFromPEM(caCert)
 
-	transport := &http.Transport{TLSClientConfig: &tls.Config{RootCAs: caCertPool}}
+	transport := &http.Transport{TLSClientConfig: &tls.Config{Certificates: []tls.Certificate{clientCert}, RootCAs: caCertPool}}
 
 	client := &http.Client{Transport: transport, Timeout: 5 * time.Second}
 
