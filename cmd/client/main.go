4b8a6a1afe867e0d5caa4e17f166f0b405725594
Kia Farhang <kfarhang0@gmail.com>
2022-05-16 18:07:21 -0500

    feature: add HTTP client that validates server cert

---
+++ cmd/client/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	caCert, err := ioutil.ReadFile("certs/ca/CA.pem")
+	if err != nil {
+		log.Fatalf("Error reading CA cert file: %s", err.Error())
+	}
+
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	transport := &http.Transport{TLSClientConfig: &tls.Config{RootCAs: caCertPool}}
+
+	client := &http.Client{Transport: transport, Timeout: 5 * time.Second}
+
+	response, err := client.Get("https://localhost:4000")
+
+	if err != nil {
+		log.Fatalf("Error calling server: %s", err.Error())
+	}
+
+	defer response.Body.Close()
+
+	bodyBytes, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatalf("Error reading response body: %s", err.Error())
+	}
+
+	fmt.Printf("Received response from server: %s\n", string(bodyBytes))
+
+}
